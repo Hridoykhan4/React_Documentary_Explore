c603357587a8f5208b6d8ee7c0efa52b5b8fe959
Hridoykhan4 <hridoykhan148385@gmail.com>
2026-06-09 22:53:54 +0600

    Refactor PreBoot component by removing unused state and form handling code

--- src/layouts/PreBoot.jsx
+++ src/layouts/PreBoot.jsx
@@ -1,26 +1,12 @@
 import { useEffect, useState } from "react";
 
 const PreBoot = () => {
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('')
-    const handleSubmit = e => {
-        e.preventDefault();
-        const formData = {
-            title,
-            body
-        }
-    }
-
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/posts`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err.message))
-    }, [title])
+   
 
     return (
         <div className="p-10">
-            <h1>React Practice</h1>
+            
+            {/* <h1>React Practice</h1>
             <form onSubmit={handleSubmit}>
              <label htmlFor="title">Title: </label>   
             <input id="title" className="input input-primary border" type="text" onChange={(e) => setTitle(e.target.value)} />
@@ -29,7 +15,7 @@ const PreBoot = () => {
             <input id="body" className="input input-primary border" type="text" onChange={(e) => setBody(e.target.value)} />
             <br /> <br />
             <button className="btn ml-2" type="submit">Submit</button>
-            </form>
+            </form> */}
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
@@ -6,12 +6,36 @@ import router from "./routers/router";
 import BookMarkProvider from "./provider/BookMarkProvider";
 import UserProvider from "./provider/UserProvider";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BookMarkProvider>
-      <UserProvider>
-        <RouterProvider router={router}></RouterProvider>
-      </UserProvider>
-    </BookMarkProvider>
-  </StrictMode>
-);
+class Hi extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString(this.props.local)
+    }
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.setState({date: new Date().toLocaleTimeString(this.props.local)}), 1000)
+  }
+  componentWillUnmount() {
+    console.log('will unmount');
+  }
+  render ()  {
+    return (
+      <div className="p-10">
+        <h2>{new Date().toLocaleTimeString(this.props.local)}</h2>
+      </div>
+    )
+  }
+}
+
+createRoot(document.getElementById('root')).render(<Hi local="bn-BD"></Hi>)
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <BookMarkProvider>
+//       <UserProvider>
+//         <RouterProvider router={router}></RouterProvider>
+//       </UserProvider>
+//     </BookMarkProvider>
+//   </StrictMode>
+// );
